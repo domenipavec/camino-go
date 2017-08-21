62de0d9aee8b82a9918f33ce417103b966f98773
matematik7 <domen@ipavec.net>
2017-08-21 10:34:34 +0200

    Hacky timezone handling

--- diary/models/diary.go
+++ diary/models/diary.go
@@ -1,10 +1,23 @@
 package models
 
 import (
+	"log"
+	"time"
+
 	"github.com/jinzhu/gorm"
 	"github.com/matematik7/gongo/authorization"
 	"github.com/matematik7/gongo/files"
 )
+
+var location *time.Location
+
+func init() {
+	var err error
+	location, err = time.LoadLocation("Europe/Ljubljana")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 type DiaryEntry struct {
 	gorm.Model
@@ -16,10 +29,19 @@ type DiaryEntry struct {
 	MapEntry   MapEntry           `valid:"-"`
 	MapEntryID uint
 	Images     []files.Image `gorm:"many2many:diary_image"`
+	Published  bool
 
 	NumComments uint `gorm:"-"`
 	Viewed      bool `gorm:"-"`
 	NewComments bool `gorm:"-"`
+}
+
+// TODO: handle timezone nicer
+func (de *DiaryEntry) AfterFind() error {
+	de.CreatedAt = de.CreatedAt.In(location)
+	de.UpdatedAt = de.UpdatedAt.In(location)
+
+	return nil
 }
 
 type Comment struct {
@@ -28,6 +50,12 @@ type Comment struct {
 	Comment      string             `gorm:"type:text" valid:"required"`
 	Author       authorization.User `valid:"-"`
 	AuthorID     uint
+}
+
+func (c *Comment) AfterFind() error {
+	c.CreatedAt = c.CreatedAt.In(location)
+
+	return nil
 }
 
 type EntryUserRead struct {
