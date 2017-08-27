925bde7e7783058c55e76ab6d3ed40e17785b46c
matematik7 <domen@ipavec.net>
2017-08-27 13:56:01 +0200

    Added etag to maps

--- maps/map.go
+++ maps/map.go
@@ -1,7 +1,11 @@
 package maps
 
 import (
+	"crypto/md5"
+	"encoding/binary"
+	"encoding/hex"
 	"net/http"
+	"strings"
 
 	"github.com/go-chi/chi"
 	"github.com/gobuffalo/packr"
@@ -47,6 +51,9 @@ func (c *Maps) ServeMux() http.Handler {
 }
 
 func (c *Maps) ViewHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: move etag handling to render in gongo
+	etag := md5.New()
+
 	var groups []models.MapGroup
 
 	query := c.DB.Preload("Entries.DiaryEntry").Order("id desc").Find(&groups)
@@ -55,17 +62,41 @@ func (c *Maps) ViewHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	gpsDataIDS := make([]uint, 0, 100)
 	filteredGroups := groups[:0]
 	for _, group := range groups {
 		if len(group.Entries) > 0 {
 			filteredGroups = append(filteredGroups, group)
 		}
+		binary.Write(etag, binary.BigEndian, group.ID)
+		binary.Write(etag, binary.BigEndian, group.UpdatedAt.UnixNano())
+		for _, entry := range group.Entries {
+			binary.Write(etag, binary.BigEndian, entry.ID)
+			binary.Write(etag, binary.BigEndian, entry.UpdatedAt.UnixNano())
+			if entry.GpsDataID != 0 {
+				gpsDataIDS = append(gpsDataIDS, entry.GpsDataID)
+			}
+		}
 	}
 
 	var gpsData []models.GpsData
-	if err := c.DB.Find(&gpsData).Error; err != nil {
+	if err := c.DB.Where("id in (?)", gpsDataIDS).Find(&gpsData).Error; err != nil {
 		c.render.Error(w, r, err)
 		return
+	}
+
+	for _, gpsEntry := range gpsData {
+		binary.Write(etag, binary.BigEndian, gpsEntry.ID)
+		binary.Write(etag, binary.BigEndian, gpsEntry.UpdatedAt.UnixNano())
+	}
+
+	etagStr := hex.EncodeToString(etag.Sum(nil))
+	w.Header().Set("Etag", etagStr)
+	if match := r.Header.Get("If-None-Match"); match != "" {
+		if strings.Contains(match, etagStr) {
+			w.WriteHeader(http.StatusNotModified)
+			return
+		}
 	}
 
 	context := render.Context{
